6935bd6921300d4b5ef947d2ef3c7500727bbc3c
Gniewogniew <jakubbajer83@gmail.com>
2017-10-12 08:16:46 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 function main() {
-    $("#addContent").click(typeTextClickButton);
-    $("#addContent").attr("disabled", true);
-    $("#itemToDo").keyup(typeTextClickButton);
     var savedList = $("#list");
+    $("#addContent").attr("disabled", true);
 
+    function eventsHandlers() {
+    $("#addContent").click(typeTextClickButton);
+    $("#itemToDo").keyup(typeTextClickButton);
+    $(savedList).on("click", ".checkBox",selectFinishedTask)
+    $(".remove").on("click", removeFinishedTask)
+    }
+    
     $(function() {
         savedList.append("<li class=item>Kupić gazetę<label><input class=checkBox type=checkbox><span></span></label></li><li class=item>Zrobić pranie<label><input class=checkBox type=checkbox><span></span></label></li><li class=item>Wstawić zmywarkę<label><input class=checkBox type=checkbox><span></span></label></li>")
         if (localStorage.getItem("#list")) {
@@ -21,14 +26,17 @@ function main() {
             $("#addContent").attr("disabled", false);
         }
         if (event.which == 13 || event.button == 0) {
-            savedList.append("<li class=item>" + $("#itemToDo").val() + "<label><input class=checkBox type=checkbox><span></span></label>" + "</li>");
+            $(addItemsOnList)
+        }
+    }
+    function addItemsOnList(){ 
+        savedList.append("<li class=item>" + $("#itemToDo").val() + "<label><input class=checkBox type=checkbox><span></span></label>" + "</li>");
             $("#itemToDo").val("");
             localStorage.setItem("#list", savedList.html());
             $("#addContent").attr("disabled", true);
-        }
-    };
+    }
 
-    $(document).on("click", ".checkBox", function() {
+    function selectFinishedTask() {
         if ($(this).is(":checked")) {
             $(this).closest("li").toggleClass("done");
             localStorage.setItem("#list", savedList.html())
@@ -37,11 +45,12 @@ function main() {
             $(this).closest("li").removeClass("done");
             localStorage.setItem("#list", savedList.html());
         }
-    });
+    }
 
-    $(document).on('click', ".remove", function() {
-        $('li.done').remove();
+    function removeFinishedTask() {
+        $("li.done").remove();
         localStorage.setItem("#list", savedList.html());
-    });
+    }
+$(eventsHandlers)
 }
 $(main)
